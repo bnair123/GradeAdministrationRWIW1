334d3dd7baec02418806cb8b7548239fb1b189cb
bnair123 <47283308+bnair123@users.noreply.github.com>
2023-05-11 20:11:58 +0200

    Basic Update

--- GradeAdministrationW1/Program.cs
+++ GradeAdministrationW1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Grade
+{
+    public double Value { get; private set; }
+    public DateTime Date { get; }
+    public int ExamCode { get; }
+    public string Note { get; }
+    public bool Frozen { get; private set; }
+
+
+
+
+}
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
+    public int StudentNumber { get; }
+    public DateTime BirthDate { get; }
+
+    
+}
+
+public class Administration
+{
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
